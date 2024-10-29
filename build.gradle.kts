65c5f2653738870cd0e1ee02a639492a5bc44974
Boy <sivertpaulsen2@gmail.com>
2024-10-29 12:52:50 +0100

    Cherry-pick: commit 8036bb4 from oraxen

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,7 +110,6 @@ allprojects {
         implementation("dev.jorel:commandapi-bukkit-shade:$commandApiVersion")
         implementation("io.th0rgal:protectionlib:1.6.2")
         //implementation("org.bstats:bstats-bukkit:3.0.0")
-        implementation("io.th0rgal:protectionlib:1.6.1")
         implementation("com.github.stefvanschie.inventoryframework:IF:0.10.12")
         implementation("com.jeff-media:custom-block-data:2.2.2")
 
